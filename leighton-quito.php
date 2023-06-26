7b474d3b301562cff59d231d194c0b08685bb137
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-06-26 00:46:12 -0500

    Simplify initial code and update func name prefix

--- leighton-quito.php
+++ leighton-quito.php
@@ -32,166 +32,47 @@ if ( ! defined('ABSPATH') ) {
 	die();
 }
 
-if ( ! defined( 'LEIGHTONQUITO_PLUGIN_BASENAME' ) ) {
-	define( 'LEIGHTONQUITO_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+if ( ! defined( 'LEIGHTON_QUITO_PLUGIN_BASENAME' ) ) {
+	define( 'LEIGHTON_QUITO_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 }
 
-// Register activation hook
-register_activation_hook( __FILE__, array( 'Leighton_Quito', 'activation__hook' ) );
-
-// Register deactivation hook
-register_deactivation_hook( __FILE__, array( 'Leighton_Quito', 'deactivation_hook' ) );
-
-/**
- * Class Leighton_Quito
- *
- * Main class to init plugin.
- *
- * @since 1.0.0
- */
-if ( ! class_exists( 'Leighton_Quito' ) ) {
-	class Leighton_Quito {
-		/**
-		 * Plugin instance
-		 *
-		 * @var null
-		 */
-		private static $instance = null;
-
-		/**
-		 * @var Leighton_Quito_Core
-		 */
-		public $leighton_quito;
-
-		/**
-		 * Return the plugin instance
-		 *
-		 * @since 1.0.0
-		 * @return Leighton_Quito
-		 */
-		public static function get_instance() {
-			if ( is_null( self::$instance ) ) {
-				$self::$instance = new self();
-			}
-
-			return self::$instance;
-		}
-
-		/**
-		 * Plugin constructor
-		 *
-		 * @since 1.0.0
-		 */
-		public function __construct() {
-			add_action( 'admin_init', array( $this, 'initialize_admin' ) );
-
-			$this->includes();
-			$this->include_vendors();
-
-			$this->init();
-			$this->load_textdomain();
-		}
-
-		/**
-		 * Called on plugin activation
-		 *
-		 * @since 1.0.0
-		 */
-		public static function activation_hook() {
-			add_option( 'leightonquito_activation_hook', 'activated' );
-		}
-
-		/**
-		 * Called on plugin deactivation
-		 *
-		 * @since 1.0.0
-		 */
-		public static function deactivation_hook() {
-			wp_clear_scheduled_hook( 'leightonquito_cleanup' );
-		}
-
-		/**
-		 * Load plugin files
-		 *
-		 * @since 1.0.0
-		 */
-		private function includes() {
-			// Core files
-			/* @noinspection PhpIncludeInspection */
-			include_once leightonquito_plugin_dir() . 'library/class-core.php';
-		}
-
-		/**
-		 * Init the plugin
-		 *
-		 * @since 1.0.0
-		 */
-		private function init() {
-			// Initialize plugin core
-			$this->leightonquito = Leighton_Quito_Core::get_instance();
-
-			// Trigger when plugin loaded
-			do_action( 'leightonquito_loaded' );
-		}
-
-		/**
-		 * Include vendors
-		 *
-		 * @since 1.0.0
-		 */
-		private function include_vendors() {
-			// PHP 5.6.0 Requirement
-			// Based on the latest version needed on vendors list
-			if ( version_compare( PHP_VERSION, '5.6.0', 'ge' ) ) {
-				/** @noinspection PhpIncludeInspection */
-				include_once leightonquito_plugin_dir() . 'vendor/autoload_psr4.php';
-			}
-		}
-
-		/**
-		 * Load language files
-		 *
-		 * @since 1.0.0
-		 */
-		private function load_textdomain() {
-			load_plugin_textdomain( 'leighton_quito', false, 'leighton-quito/languages');
-		}
-	}
-}
-
-if ( ! function_exists( 'leightonquito' ) ) {
-	function leightonquito() {
-		return Leighton_Quito::get_instance();
-	}
-
+if ( function_exists( 'leighton_quito' ) ) {
 	/**
-	 * Init the plugin and load the plugin instance
+	 * Deactivates the plugin and stops loading the plugin instance
 	 *
 	 * @since 1.0.0
 	 */
-	add_action( 'plugins_loaded', 'leightonquito' );
+	if ( ! function_exists( 'leighton_quito_deactivate' ) ) {
+		function leighton_quito_deactivate() {
+			deactivate_plugins( LEIGHTON_QUITO_PLUGIN_BASENAME );
+		}
+	}
+
+	add_action( 'admin_init', 'leighton_quito_deactivate' );
+
+	return;
 }
 
-if ( ! function_exists( 'leightonquito_plugin_url' ) ) {
+if ( ! function_exists( 'leighton_quito_plugin_url' ) ) {
 	/**
 	 * Return plugin URL
 	 *
 	 * @since 1.0.0
 	 * @return string
 	 */
-	function leightonquito_plugin_url() {
+	function leighton_quito_plugin_url() {
 		return trailingslashit( plugin_dir_url( __FILE__ ) );
 	}
 }
 
-if ( ! function_exists( 'leightonquito_plugin_dir' ) ) {
+if ( ! function_exists( 'leighton_quito_plugin_dir' ) ) {
 	/**
 	 * Return plugin path
 	 *
 	 * @since 1.0.0
 	 * @return string
 	 */
-	function leightonquito_plugin_dir() {
+	function leighton_quito_plugin_dir() {
 		return trailingslashit( plugin_dir_path( __FILE__ ) );
 	}
 }
