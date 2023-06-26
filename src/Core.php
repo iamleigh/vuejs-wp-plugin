0b3e3badb8479e73c1434be077482a1a4803da43
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-06-26 09:36:03 -0500

    Configure base namespace and init core class

--- src/Core.php
+++ src/Core.php
@@ -1,5 +1,39 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
 
+namespace LeightonQuito;
+
+/**
+ * Class Core
+ *
+ * @since 1.0.0
+ */
+class Core {
+	/**
+	 * Path to plugin directory
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Without trailing slash
+	 */
+	public $plugin_path;
+
+	/**
+	 * URL to plugin directory
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Without trailing slash
+	 */
+	public $plugin_url;
+
+	/**
+	 * Core constructor
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$this->plugin_path = leighton_quito_plugin_dir();
+		$this->plugin_url = leighton_quito_plugin_url();
+		$this->assets_url  = $this->plugin_url . '/assets';
+	}
+}
