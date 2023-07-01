d4e5a15a8bc6793cfeb9ce2672e2182537425b25
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-01 03:00:01 -0500

    feat: Setup index storage

--- assets/vue/admin.js
+++ assets/vue/admin.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import Router from "vue-router";
-// import store from "../store/index";
+import store from "../store/index";
 import App from "./App.vue";
 import TabNavigation from "./components/tabs/Navigation.vue";
 import TableTab from "./components/tabs/TableTab.vue";
@@ -33,7 +33,7 @@ const router = new VueRouter({
 
 new Vue({
 	el: "#lq-admin-app",
-	//   store,
+	store,
 	router,
 	render: (h) => h(App),
 });
