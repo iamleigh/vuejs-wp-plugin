aa3b292c9dc84080b3364be6ff2f9c6a356ccd44
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-01 02:50:04 -0500

    feat: Build navigation links

---
+++ assets/vue/components/tabs/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+	<ul class="tab-menu">
+		<li><router-link to="/">Table</router-link></li>
+		<li><router-link to="/">Graph</router-link></li>
+		<li><router-link to="/">Settings</router-link></li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: 'TabNavigation'
+}
+</script>
