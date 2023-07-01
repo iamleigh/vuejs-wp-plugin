806a3cd8dbecbd9a2bd45cab7b344b11c4fdb838
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-01 02:40:22 -0500

    feat: Draw app template base

---
+++ assets/vue/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<Fragment>
+		<div role="tablist">
+			<button role="tab">Table</button>
+			<button role="tab">Graph</button>
+			<button role="tab">Settings</button>
+		</div>
+		<div>Table content</div>
+		<div>Graph content</div>
+		<div>Settings content</div>
+	</Fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment';
+
+export default {
+    name: 'App',
+	components: {
+		Fragment
+	}
+}
+</script>
