45176c1aadacfb0d3976dd9b88c57d68fb5be986
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 00:59:59 -0500

    fix: Update tabs route

--- assets/vue/components/tabs/Navigation.vue
+++ assets/vue/components/tabs/Navigation.vue
@@ -1,8 +1,8 @@
 <template>
 	<ul class="lq-tabs__menu">
 		<li><router-link to="/">Table</router-link></li>
-		<li><router-link to="/">Graph</router-link></li>
-		<li><router-link to="/">Settings</router-link></li>
+		<li><router-link to="/graph">Graph</router-link></li>
+		<li><router-link to="/settings">Settings</router-link></li>
 	</ul>
 </template>
 
