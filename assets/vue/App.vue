268964c0c7f58c09b4c9a295d4ba1aa580b29a11
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 03:21:43 -0500

    fix: Route cannot find object wrapped in Fragment

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -1,18 +1,16 @@
 <template>
-	<fragment>
+	<div>
 		<router-view name="tab"></router-view>
 		<router-view></router-view>
-	</fragment>
+	</div>
 </template>
 
 <script>
-import { Fragment } from 'vue-fragment';
 import TabNavigation from './components/tabs/Navigation.vue'
 
 export default {
     name: 'App',
 	components: {
-		Fragment,
 		'tab-navigation': TabNavigation
 	}
 }
