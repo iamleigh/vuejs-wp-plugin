606f8b720546f045ca3574ed8aa64aef31c4e460
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 03:39:26 -0500

    fix: Improve tabs menu markup
    Defines new markup for the tabs menu that allows a better navigation for screen reader clients.

--- assets/vue/components/tabs/Navigation.vue
+++ assets/vue/components/tabs/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
-	<ul class="lq-tabs__menu">
-		<li><router-link to="/">Table</router-link></li>
-		<li><router-link to="/graph">Graph</router-link></li>
-		<li><router-link to="/settings">Settings</router-link></li>
-	</ul>
+	<div role="tablist" class="leighton-quito-tabs__menu">
+		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/">Table</router-link>
+		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/graph">Graph</router-link>
+		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/settings">Settings</router-link>
+	</div>
 </template>
 
 <script>
