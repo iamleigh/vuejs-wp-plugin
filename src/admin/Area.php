f84a047ebf25cfcb8660d93088fea9ace9c95ec5
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 02:32:59 -0500

    fix: Move admin header outside page template

--- src/admin/Area.php
+++ src/admin/Area.php
@@ -74,6 +74,9 @@ class Area {
 	protected function hooks() {
 		add_action( 'admin_menu', [ $this, 'admin_menu' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'register_assets' ] );
+
+		// Outputs admin header
+		add_action( 'in_admin_header', [ $this, 'header_template' ], 100 );
 	}
 
 	/**
@@ -152,6 +155,20 @@ class Area {
 	}
 
 	/**
+	 * Check whether we are on an admin page.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	public function is_admin_page() {
+		$cur_page    = isset( $_GET['page'] ) ? sanitize_key( $_GET['page'] ) : '';
+		$check       = self::SLUG;
+
+		return is_admin() && $cur_page === $check;
+	}
+
+	/**
 	 * Render admin page
 	 *
 	 * @since 1.0.0
@@ -160,10 +177,36 @@ class Area {
 		?>
 
 		<div class="wrap lq-plugin">
-			<h1>Leighton Quito</h1>
-
 			<div id="lq-admin-app"></div>
 		</div>
+
+		<?php
+	}
+
+	/**
+	 * Render admin header
+	 *
+	 * @since 1.0.0
+	 */
+	public function header_template() {
+		// Bail if we're not on a plugin page.
+		if ( ! $this->is_admin_page() ) {
+			return;
+		}
+
+		do_action( 'leighton_quito_header_template_before' );
+		?>
+
+			<div class="lq-header">
+				<div class="lq-header__image lq-logo" aria-hidden="true">
+					<img src="<?php echo esc_url( $this->plugin_url ); ?>assets/images/logo.svg" alt="Leighton Quito" class="lq-logo__image" />
+				</div>
+
+				<div class="lq-header__slogan">
+					<h1 class="lq-header__title">Leighton Quito</h1>
+					<p class="lq-header__subtitle">a <strong>vue<span>js</span></strong> developer applicant challenge</p>
+				</div>
+			</div>
 
 		<?php
 	}
