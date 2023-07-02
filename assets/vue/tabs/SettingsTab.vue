0dbde4c82ca68bd7b433bcdb147208494958e110
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 17:10:59 -0500

    fix: Improve settings content markup

--- assets/vue/tabs/SettingsTab.vue
+++ assets/vue/tabs/SettingsTab.vue
@@ -7,47 +7,66 @@
 
 		<settings-row>
 			<template v-slot:left>
-				<label>Rows</label>
+				<label for="lq-table-rows" id="lq-table-rows__label">Rows</label>
 			</template>
 
 			<template v-slot:right>
-				<input type="number" min="1" max="5" value="5" />
-				<span>Limit the number of rows to display in the table.</span>
+				<div class="leighton-quito-field">
+					<span class="leighton-quito-field__label">Limit the number of rows to display in the table.</span>
+					<input
+						type="number"
+						min="1"
+						max="5"
+						value="5"
+						id="lq-table-rows"
+						class="small-text"
+						aria-labelledby="lq-table-rows__label"
+						aria-describedby="lq-table-rows__desc" />
+					<span id="lq-table-rows__desc" class="leighton-quito-field__desc">The allowed rows value must be between 1 and 5.</span>
+				</div>
 			</template>
 		</settings-row>
 
 		<settings-row title="Timestamp">
 			<template v-slot:right>
 				<div role="radiogroup" class="leighton-quito-radio__group">
-					<div class="leighton-quito-radio">
-						<input type="radio" id="unix" name="timestamp" value="unix">
-						<label>Unix</label>
-					</div>
-
-					<div class="leighton-quito-radio">
+					<label class="leighton-quito-radio">
 						<input type="radio" id="custom" name="timestamp" value="custom">
-						<label>Custom</label>
-					</div>
+						Date
+					</label>
+
+					<label class="leighton-quito-radio">
+						<input type="radio" id="unix" name="timestamp" value="unix">
+						Unix
+					</label>
 				</div>
 			</template>
 		</settings-row>
 
 		<settings-row title="Emails">
 			<template v-slot:right>
-				<div class="">
-					<div class="">
-						<input type="email" value="name@email.com" readonly />
-						<button class="leighton-quito-icon-button">X</button>
+				<div class="leighton-quito-field">
+					<div class="leighton-quito-field leighton-quito-field--inline">
+						<input type="email" value="name@email.com" class="leighton-quito-input leighton-quito-field__item" readonly />
+						<action label="Remove email" icon="no-alt" design="secondary" />
 					</div>
 				</div>
 
-				<div class="">
-					<label class="">Add an email address</label>
-					<input type="email" />
-					<button class="leighton-quito-button">Add Email</button>
+				<div class="leighton-quito-field">
+					<span class="leighton-quito-field__label">Add an email address</span>
+					<div
+						role="none"
+						class="leighton-quito-field leighton-quito-field--inline">
+						<input type="email" class="leighton-quito-input leighton-quito-field__item" />
+						<action label="Add Email" />
+					</div>
 				</div>
 			</template>
 		</settings-row>
+
+		<button class="leighton-quito-button leighton-quito-button--primary leighton-quito-button--orange">
+			Save Settings
+		</button>
 	</div>
 </template>
 
