3b6168cbe103b77a20fa05946cd66502f431d606
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-02 03:59:10 -0500

    fix: Use `v-for` to map tabs

--- assets/vue/components/tabs/Navigation.vue
+++ assets/vue/components/tabs/Navigation.vue
@@ -1,13 +1,29 @@
 <template>
 	<div role="tablist" class="leighton-quito-tabs__menu">
-		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/">Table</router-link>
-		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/graph">Graph</router-link>
-		<router-link role="tab" class="leighton-quito-tabs__item" ariaCurrentValue="true" to="/settings">Settings</router-link>
+		<router-link
+			v-for="tab in tabsList"
+			v-bind:key="tab.id"
+			:to={path:tab.router}
+			role="tab"
+			class="leighton-quito-tabs__item"
+			activeClass="active"
+			ariaCurrentValue="true">
+			{{ tab.name }}
+		</router-link>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'TabNavigation'
+	name: 'TabNavigation',
+	data () {
+		return {
+			tabsList: [
+				{name: 'Table', router: '/', id: 1},
+				{name: 'Graph', router: '/graph', id: 2},
+				{name: 'Settings', router: '/settings', id: 3}
+			]
+		}
+	}
 }
 </script>
