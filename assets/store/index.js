80e1b43b48b8103d44452c668dac7b8c8e52deaa
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-03 21:12:41 -0500

    fix: Missing index modules

--- assets/store/index.js
+++ assets/store/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { actions } from './actions';
-import { getters } from './getters';
-import { mutations } from './mutations';
+import { actions } from './modules/actions';
+import { getters } from './modules/getters';
+import { mutations } from './modules/mutations';
 
 Vue.use(Vuex);
 
