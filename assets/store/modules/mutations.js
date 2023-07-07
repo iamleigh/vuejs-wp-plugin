53206d9e146a9645945e3a08f2846257d8194b31
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 17:30:01 -0500

    feat(store): Create add and remove email mutation

--- assets/store/modules/mutations.js
+++ assets/store/modules/mutations.js
@@ -1,10 +1,20 @@
 export const mutations = {
+	ADD_EMAIL: (state, payload) => {
+		state.settings.general.emails.push(payload);
+	},
+
+	REMOVE_EMAIL: (state, payload) => {
+		state.settings.general.emails = state.settings.general.emails.filter((item) => item.id !== payload);
+	},
+
 	UPDATE_SETTINGS: (state, payload) => {
 		state.settings.general = payload;
 	},
+
 	SAVED_SETTINGS: (state) => {
 		state.saving = false;
 	},
+
 	SAVING_SETTINGS: (state) => {
 		state.saving = true;
 	},
