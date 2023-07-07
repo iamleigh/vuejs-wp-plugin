6e452372a1dba03ba77f9f7dacf82b550072990c
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 06:26:07 -0500

    fix(ui-notice): Create component

---
+++ assets/vue/components/UI/UINotice.vue
@@ -0,0 +1,32 @@
+<template>
+	<div
+		role="alert"
+		class="leighton-quito-notice"
+		:class="setNoticeClass">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UINotice',
+	computed: {
+		setNoticeClass: function () {
+			let setClass = '';
+
+			switch (this.type) {
+				case 'loading':
+				case 'error':
+					setClass = 'leighton-quito-notice--' + this.type;
+					break;
+
+				default:
+					// do nothing
+					break;
+			}
+
+			return setClass;
+		}
+	}
+}
+</script>
