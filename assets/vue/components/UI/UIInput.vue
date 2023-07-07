27d255c776673e8572d806a1483004d5efea93c1
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 04:51:24 -0500

    feat(ui-input): Create component

---
+++ assets/vue/components/UI/UIInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<input
+		:value="value"
+		class="leighton-quito-input"
+		:class="setInputClass"
+		@input="$emit('changeValue', $event.target.value)" />
+</template>
+
+<script>
+export default {
+	name: 'UIInput',
+	model: {
+		prop: 'value',
+		event: 'changeValue'
+	},
+	props: {
+		value: {
+			Type: String,
+			default: ''
+		}
+	},
+	computed: {
+		setInputClass: function () {
+			if ('radio' === this.type) {
+				return 'small-text';
+			}
+
+			return;
+		}
+	}
+}
+</script>
