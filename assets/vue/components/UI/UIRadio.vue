de857b3b9b2f114fb1e0be9a74c6e4a0b14eca6e
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 04:51:18 -0500

    feat(ui-radio): Create component

---
+++ assets/vue/components/UI/UIRadio.vue
@@ -0,0 +1,36 @@
+<template>
+	<label class="leighton-quito-radio">
+		<input
+			type="radio"
+			v-bind="$attrs"
+			:checked="isChecked"
+			:value="value"
+			@change="$emit('change', $event.target.value)"/>
+		<slot></slot>
+	</label>
+</template>
+
+<script>
+export default {
+	name: 'UIRadio',
+	inheritAttrs: false,
+	model: {
+		prop: 'modelValue',
+		event: 'change'
+	},
+	props: {
+		value: {
+			Type: String,
+			default: null
+		},
+		modelValue: {
+			default: ''
+		}
+	},
+	computed: {
+		isChecked: function () {
+			return this.modelValue === this.value;
+		}
+	}
+}
+</script>
