87f0170d6c346e8559a240a19be129c9d9b6f9c6
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 06:18:35 -0500

    fix(data-block): Create component

---
+++ assets/vue/components/Data/DataBlock.vue
@@ -0,0 +1,23 @@
+<template>
+	<div role="none" class="leighton-quito-data-block">
+		<h3 v-if="hasTitle">{{ title }}</h3>
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DataBlock',
+	props: {
+		title: {
+			Type: String,
+			default: null
+		}
+	},
+	computed: {
+		hasTitle: function () {
+			return this.title ? true : false;
+		}
+	}
+}
+</script>
