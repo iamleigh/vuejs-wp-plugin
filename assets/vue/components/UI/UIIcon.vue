4f14008e4ca457e580031007b0b9d1a65fe53354
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 01:21:19 -0500

    feat(ui-icon): Create component

---
+++ assets/vue/components/UI/UIIcon.vue
@@ -0,0 +1,23 @@
+<template>
+	<span
+		class="dashicons"
+		:class="icon"
+		aria-hidden="true" />
+</template>
+
+<script>
+export default {
+	name: 'UIIcon',
+	props: {
+		name: {
+			Type: String,
+			required: true
+		}
+	},
+	computed: {
+		icon: function () {
+			return `dashicons-${this.name}`;
+		}
+	}
+}
+</script>
