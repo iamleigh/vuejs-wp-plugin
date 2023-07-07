543650fc7ee102c85c22cde6edd2345deadce71a
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-07 17:30:23 -0500

    feat(email-preview): Create component

---
+++ assets/vue/components/Email/EmailPreview.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<span>{{ emailItem.value }}</span>
+		<span>{{ emailItem }}</span>
+		<UIButton
+			type="button"
+			buttonIcon="trash"
+			buttonDesign="tertiary"
+			@button-click="REMOVE_EMAIL(emailItem.id)">
+			Remove Email
+		</UIButton>
+	</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import UIButton from '../UI/UIButton';
+
+export default {
+	name: 'EmailPreview',
+	components: {
+		UIButton
+	},
+	props: {
+		emailItem: {
+			Type: Object,
+			required: true
+		}
+	},
+	methods: {
+		...mapActions([ 'REMOVE_EMAIL' ])
+	}
+}
+</script>
