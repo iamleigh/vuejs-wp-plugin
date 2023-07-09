c512212147caf438614ab37dc0d06d9f9543a5b1
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2023-07-09 01:00:56 -0500

    fix(ui-section): Create component

---
+++ assets/vue/components/UI/UISection.vue
@@ -0,0 +1,32 @@
+<template>
+	<div role="none" class="leighton-quito-section">
+		<h3 v-if="hasTitle" class="leighton-quito-section__title">
+			{{ title }}
+		</h3>
+
+		<div role="none" class="leighton-quito-section__content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UISection',
+	props: {
+		title: {
+			Type: String,
+			default: null
+		}
+	},
+	computed: {
+		hasTitle: function () {
+			if (this.title) {
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+</script>
